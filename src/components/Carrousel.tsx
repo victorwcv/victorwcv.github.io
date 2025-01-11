b4b6f7ee02ab1415308f1b3f7547401056d08815
William <victorwccv@gmail.com>
2025-01-11 16:11:22 -0500

    feat: create carrousel component

---
+++ src/components/Carrousel.tsx
@@ -0,0 +1,90 @@
+import { useAnimation, motion } from 'motion/react';
+import { FC, useEffect, useRef, useState } from 'react';
+
+export interface CarouselItem {
+  id: number;
+  src: string;
+  title: string;
+  link?: string;
+  description?: string;
+  tags?: string[];
+}
+
+interface Props {
+  items: CarouselItem[];
+}
+
+const Carrousel: FC<Props> = ({ items }) => {
+  const [isPaused, setIsPaused] = useState(false);
+  const [isMovingForward, setIsMovingForward] = useState(true);
+  const containerRef = useRef(null);
+  const controls = useAnimation();
+  const positionRef = useRef(0);
+
+  useEffect(() => {
+    const maxOffset = 320 * (items.length - 3); // Desplazamiento máximo permitido
+    const moveCarousel = () => {
+      let x = positionRef.current;
+
+      if (isMovingForward) {
+        x -= 320; // Mover hacia adelante
+        if (Math.abs(x) >= maxOffset) {
+          setIsMovingForward(false); // Cambiar dirección
+        }
+      } else {
+        x += 320; // Mover hacia atrás
+        if (x >= 0) {
+          setIsMovingForward(true); // Cambiar dirección
+        }
+      }
+
+      positionRef.current = x;
+      controls.start({ x, transition: { duration: 4, ease: 'linear' } });
+    };
+
+    const interval = setInterval(() => {
+      if (!isPaused) moveCarousel();
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, [controls, items.length, isPaused, isMovingForward]);
+
+  return (
+    <div
+      className="carousel-container relative lg:w-[960px] w-[320px] overflow-hidden mx-auto"
+      onMouseEnter={() => setIsPaused(true)} // Pausar al hacer hover
+      onMouseLeave={() => setIsPaused(false)} // Reanudar al salir
+    >
+      <motion.div
+        ref={containerRef}
+        className="flex items-center"
+        animate={controls}
+        initial={{ x: 0 }}
+        style={{
+          display: 'flex',
+          width: `${300 * items.length}px`,
+        }}
+      >
+        {items.map((item, index) => (
+          <div
+            className="hover:cursor-pointer"
+            key={index}
+            style={{
+              minWidth: '300px', // Ajusta al tamaño de cada imagen
+              marginInline: '10px',
+            }}
+          >
+            <img
+              src={item.src}
+              alt={`Image ${index + 1}`}
+              style={{ width: '100%' }}
+              className="block w-full h-auto shadow"
+            />
+          </div>
+        ))}
+      </motion.div>
+    </div>
+  );
+};
+
+export default Carrousel;
